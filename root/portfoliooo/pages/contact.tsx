f551398a337c36a54e3f29cd5bbd9024a651f813
CuongNguyen <>
2024-01-19 07:53:28 +0900

    edit-pages

---
+++ root/portfoliooo/pages/contact.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ContainerBlock from "../components/ContainerBlock";
+import Contact from "../components/Contact";
+
+const ContactPage: React.FC = () => {
+  return (
+    <ContainerBlock>
+      <Contact />
+    </ContainerBlock>
+  );
+};
+
+export default ContactPage;
---
+++ root/portfoliooo/pages/experience.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ContainerBlock from "../components/ContainerBlock";
+import Experience from "../components/Experience";
+
+const ExperiencePage: React.FC = () => {
+  return (
+    <ContainerBlock title="Experience - Manu Arora">
+      <Experience />
+    </ContainerBlock>
+  );
+};
+
+export default ExperiencePage;
